0a3181d7de3da3d2d84796ecc735571ebf13d282
Caslav Bakic <caslav.bakic@synergysuite.com>
2023-09-22 12:00:58 +0200

    added some optional query params for fetching products

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -6,16 +6,31 @@ const Category = require('../models/categoryModel');
 
 exports.getAllProducts = catchAsync(async (req, res, next) => {
   let productsQuery = Product.find().select('-__v');
-  if (req.query.limit != undefined)
-    productsQuery = productsQuery.limit(req.query.limit);
+  let pagesCount = 0;
+  let documentsCount = await Product.countDocuments();
+  let page = 1;
+  let limit = 10;
+  if (req.query.limit != undefined) {
+    limit = req.query.limit;
+    productsQuery = productsQuery.limit(limit);
+    pagesCount = documentsCount / limit;
+  }
+  if (req.query.page != undefined) {
+    page = req.query.page;
+  }
+  if (documentsCount % limit != 0) pagesCount++;
+  productsQuery = productsQuery.skip((page - 1) * limit);
   const products = await productsQuery.exec();
   if (!products)
     return res
       .status(400)
       .json({ status: 'not ok', message: 'greska u uzimanju producta' });
-  res
-    .status(200)
-    .json({ status: 'ok', message: 'uzeti producti', data: products });
+  res.status(200).json({
+    status: 'ok',
+    message: 'uzeti producti',
+    brojStranica: pagesCount,
+    data: products,
+  });
 });
 
 exports.getProductInfo = catchAsync(async (req, res, next) => {
